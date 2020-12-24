d19a51c29e7d9f43426ce2a325e8876b9d8b943c
danielenricocahall <danielenricocahall@gmail.com>
2020-12-24 18:52:19 -0500

    change process numbe

--- train.py
+++ train.py
@@ -28,24 +28,22 @@ def generate_images(batch_size: int, shape: tuple):
     while True:
         random.shuffle(files)
         for batch in get_batch(files, batch_size, shape):
-            n_classes = batch.shape[0]
             w, h = shape[0], shape[1]
-            categories = rng.choice(n_classes, size=(batch_size,), replace=False)
-            pairs = [np.zeros((batch_size, w, h, 3)) for i in range(2)]
+            pairs = [np.zeros((batch_size, w, h, 3)) for _ in range(2)]
             targets = np.zeros((batch_size,))
             targets[batch_size // 2:] = 1
             for i in range(batch_size):
-                category = categories[i]
+                category = i
                 pairs[0][i, :, :, :] = batch[category, ]
                 # pick images of same class for 1st half, different for 2nd
-                category_2 = category if i >= batch_size // 2 else (category + rng.randint(1, n_classes)) % n_classes
+                category_2 = category if i >= batch_size // 2 else (category + random.randrange(1, batch_size)) % batch_size
                 pairs[1][i, :, :, :] = batch[category_2, ]
             yield pairs, targets
 
 
 if __name__ == "__main__":
     shape = (614, 421, 3)
-    epochs = 10
+    epochs = 20
     batch_size = 16
     model = get_siamese_model(shape)
     optimizer = Adam(lr=0.00006)
@@ -57,5 +55,5 @@ if __name__ == "__main__":
                         steps_per_epoch=len(os.listdir('./cards/')) / batch_size,
                         use_multiprocessing=True,
                         callbacks=[checkpoint],
-                        workers=4,
+                        workers=os.cpu_count(),
                         shuffle=True)
